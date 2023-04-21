9a918e21186ba21009adf2eed593ed13692413ac
soyeonee <busanpsy1@gmail.com>
2023-04-21 10:37:39 -0400

    add mongoDB, mongoose

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
-import express, { application } from "express";
+import "./db";
+import "./models/Video";
+import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
