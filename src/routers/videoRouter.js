15a1a758bb049c893d1d472e828ba6493009ec9d
soyeonee <busanpsy1@gmail.com>
2023-04-25 16:28:53 -0400

    delete unnecessary lines

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -11,7 +11,7 @@ videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
-videoRouter.get("/:id/delete", deleteVideo);
+
 
 
 export default videoRouter;
