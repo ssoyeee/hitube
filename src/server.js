52bf0e9b6e8ea4c1190e7e728ac148af4f76d2b3
soyeonee <busanpsy1@gmail.com>
2023-04-28 15:42:01 -0400

    User Authentication: Sessions and Cookies

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { application } from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -15,6 +16,27 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({extended: true}));
+//before router
+app.use(
+    session({
+        secret: "Hello",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+app.use((req, res, next) => {
+    req.sessionStore.all((error, sessions) => {
+        console.log(sessions);
+        next();
+    });
+});
+
+app.get("/add-one", (req, res, next) => {
+    req.session.potato += 1;
+    return res.send(`${req.session.id}\n${req.session.potato}`);
+});
+
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
