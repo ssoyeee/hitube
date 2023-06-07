cd7e3e0c969b752c8fcf7ff4d2cb4ead60362e4c
ssoyeee <busanpsy1@gmail.com>
2023-06-06 22:52:08 -0400

    MongoStore: Store session

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express, { application } from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
