5605c8034040c9a951b5311dd0458101e4592044
ssoyeee <busanpsy1@gmail.com>
2023-06-18 16:18:09 -0400

    Get user info using Github API

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import fetch from 'node-fetch';
 import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", {pageTitle: "Join"});
@@ -79,14 +80,28 @@ export const finishGithubLogin = async (req, res) => {
     };
     const params = new URLSearchParams(config).toString();
     const finalUrl = `${baseUrl}?${params}`;
-    const data = await fetch(finalUrl, {
-        method: "POST",
-        headers:{
-            Accept: "application/json",
-        },
-    });
-    const json = await data.json();
-    console.log(json);
+    const tokenRequest = await (
+        await fetch(finalUrl, {
+            method: "POST",
+            headers:{
+                Accept: "application/json",
+            },
+        })
+    ).json();
+    if("access_token" in tokenRequest){
+        const {access_token} = tokenRequest;
+        const userRequest = await (
+            await fetch("https://api.github.com/user", {
+                headers: {
+                    Authorization: `token ${access_token}`,
+                },
+            })
+        ).json();
+        console.log(userRequest);
+    }else{
+        return res.redirect("/login"); 
+    }
+    //res.send(JSON.stringify(json));
 };
 export const edit = (req,res) => res.render("edituser", {pageTitle: "Edit User"});
 export const remove = (req,res) => res.render("removeuser", {pageTitle: "Remove User"});
